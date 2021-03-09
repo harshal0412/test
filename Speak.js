36e1e74573692dd00717d4c319cf85a3c87e0ecf
Harshal Jain <harshaludr.udr@gmail.com>
2021-03-09 13:14:26 +0530

    woww

---
+++ Speak.js
@@ -0,0 +1,12 @@
+
+(function(window){
+var helloSpeaker = new Object();
+
+var speakWord = "Hello";
+
+	 helloSpeaker.speak=function(names){
+  console.log(speakWord + " " + name);
+			}
+			window.hellospeaker=hellospeaker;
+})(window);
+
